e2e920b2974d89126f41ea9977a99264f0e0b3e7
Leonardogregoriocs <leonardogregoriocs@gmail.com>
2023-06-14 17:42:45 -0300

    Edit file docker-compose and connect db

--- app/db/database.py
+++ app/db/database.py
@@ -7,10 +7,27 @@ class DB:
     def connect():
         connection = psycopg2.connect(
             database="dataclient",
-            host="192.168.64.2",
+            host="172.18.0.2",
             user="neowayuser",
             password="neoway"
         )
 
         cursor = connection.cursor()
+
+        # cursor.execute(
+        # """CREATE TABLE base_teste(
+        #     cpf varchar(11) NOT NULL,
+        #     PRIMARY KEY (cpf),
+        #     private smallint NULL,
+        #     incompleto smallint NULL,
+        #     data_ultima_compra varchar(10) NULL,
+        #     ticket_medio float NULL,
+        #     ticket_ultima_compra float NULL,
+        #     loja_mais_frequente varchar(14) NULL,
+        #     loja_ultima_compra varchar(14) NULL);
+        #     """
+        # )
+
+        connection.commit()
+
         return cursor
--- app/db/queries.py
+++ app/db/queries.py
@@ -8,17 +8,20 @@ class Queries:
 
     def create_table(self):
         cursor.execute(
-        """CREATE TABLE base_teste
-            cpf serial NOT NULL,
-            PRIMARY KEY (cpf,
+        """CREATE TABLE base_teste(
+            cpf varchar(11) NOT NULL,
+            PRIMARY KEY (cpf),
             private smallint NULL,
             incompleto smallint NULL,
-            data_ultima_compra text(10) NULL,
-            ticket_medio double precision NULL,
-            ticket_ultima_compra double precision NULL,
-            loja_mais_frequente integer(14) NULL,
-            loja_ultima_compra integer(14) NULL;"""
+            data_ultima_compra varchar(10) NULL,
+            ticket_medio float NULL,
+            ticket_ultima_compra float NULL,
+            loja_mais_frequente varchar(14) NULL,
+            loja_ultima_compra varchar(14) NULL);
+            """
         )
+
+        cursor.commit()
 
     def insert_data(self):
         cursor.execute(
