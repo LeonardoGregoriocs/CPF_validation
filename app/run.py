63971fb4a6d1e788a0d3b2e3f9a1a8b090e5ed04
Leonardogregoriocs <leonardogregoriocs@gmail.com>
2023-06-15 17:16:41 -0300

    Add function sleep for wait database is ready

--- app/run.py
+++ app/run.py
@@ -1,8 +1,12 @@
+import time
+
 from db.queries import Queries
 from service.analyze_data_service import AnalyzeDataService
 
 
 try:
+    time.sleep(15)
+
     queries = Queries()
     queries.create_table()
 
