24b1bf7a2580c10370b1f600b36e53c9b957c9c9
Leonardogregoriocs <leonardogregoriocs@gmail.com>
2023-06-18 14:05:29 -0300

    Add unit test

--- test/test_analyze_data_service.py
+++ test/test_analyze_data_service.py
@@ -4,6 +4,30 @@ def test_validator_cpf_when_cpf_is_valid():
     analyze_data = AnalyzeDataService()
 
     cpf = '12365487982'
-    result = analyze_data.validator_cpf(cpf)
+    result = analyze_data.clean_and_validator_cpf(cpf)
 
-    assert result is True
+    assert result == cpf
+
+def test_validator_cpf_when_cpf_is_not_valid():
+    analyze_data = AnalyzeDataService()
+
+    cpf = '123654879'
+    result = analyze_data.clean_and_validator_cpf(cpf)
+
+    assert result is False
+
+def test_validator_cnpj_when_cnpj_is_valid():
+    analyze_data = AnalyzeDataService()
+
+    cnpj = '21340472000163'
+    result = analyze_data.clean_and_validator_cnpj(cnpj)
+
+    assert result == cnpj
+
+def test_validator_cnpj_when_cnpj_is_not_valid():
+    analyze_data = AnalyzeDataService()
+
+    cnpj = '21340472000163'
+    result = analyze_data.clean_and_validator_cnpj(cnpj)
+
+    assert result is False
